590cd74bf1d43819ecdb86da46f9fdb0c1b43ff9
wangjinhan <wangjinhan@lixiang.com>
2022-08-16 10:12:43 +0800

    Ver 0.01

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-
+#include <opencv2/opencv.hpp>
 int main()
 {
-        std::cout << "hello, world" << std::endl;
-        return 0;
+    cv::Mat img = cv::imread("./res/front.bmp");
+    cv::imshow("img", img);
+    cv::waitKey(0);
+    std::cout << "hello, world" << std::endl;
+    return 0;
 }
